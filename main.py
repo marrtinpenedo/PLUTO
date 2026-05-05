e35971d7f05d77598afb0b3eb17d4ef8b7b2a0ba
Lucas Martínez Calveiro <lucasmtnz23@gmail.com>
2026-05-05 21:52:06 +0200

    updated: Modificado para poder acceder desde la red local

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     application.launch(
         server_name="0.0.0.0",
         server_port=7860,
-        share=False,
+        share=True,
         show_error=True,
         favicon_path=None,
     )
